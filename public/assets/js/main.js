c51f78a1be3f754a8e3d427c33c5a02447f21cc2
Tisha Salodiya <tishachalodiya@gmail.com>
2026-02-20 13:45:38 +0530

    image-faq-section

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20543,11 +20543,12 @@ var Accordion = /*#__PURE__*/function () {
       $(document).ready(function () {
         // Open the first child by default
         $('.closet-header').first().addClass('active').next('.closet-content').slideDown();
-
+        $('.closet-content').first().addClass('active-content');
         // Handle click events for closet headers
         $('.closet-header').click(function () {
           $(this).toggleClass('active').next('.closet-content').slideToggle();
           $('.closet-header').not(this).removeClass('active').next('.closet-content').slideUp();
+          $('.closet-content').toggleClass('active-content');
         });
       });
     }
